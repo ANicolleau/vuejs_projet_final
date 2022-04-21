bc2c9121d1dfcdd5079a33cc2aa3a398832e0ba5
FlorianLeveil <florian.leveil@ynov.com>
2022-04-21 02:31:12 +0200

    first commit init beer compenent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+		<div class="row mt-3">
+			<div class="col bg-info">
+			</div>
+			<div class="col">
+				<BeerDisplay/>
+			</div>
+		</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BeerDisplay from "@/components/BeerDisplay";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BeerDisplay
   }
 }
 </script>
@@ -21,6 +26,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/BeerDisplay.vue
@@ -0,0 +1,134 @@
+<template>
+	<div class="h-100 w-100 rounded-3 bg-light" style="">
+		<div class="row">
+			<div class="col-4 bg-warning"><img src="{{image_url}}" class="img-fluid border-dark" style="max-width: 200px;" alt="Responsive image"></div>
+			<div class="col-8">
+				<h2>{{name}}</h2>
+				<div class="text-start">
+					<span class="label label-default bg-dark text-light rounded-3 p-1 m-1 ">{{tagline}}</span>
+				</div>
+				<div class="text-start mt-4">
+					<span class="fw-bold" style="font-size: 20px">Brassé la première fois:</span><span style="font-size: 20px; margin-left: 10px">{{first_brewed}}</span>
+				</div>
+				<div class="text-start mt-4">
+					<span class="fw-bold" style="font-size: 20px">Description:</span><span style="font-size: 20px; margin-left: 10px">{{description}}</span>
+				</div>
+			</div>
+		</div>
+		<div class="text-start mt-4">
+			<h3>Informations:</h3>
+		</div>
+		<div class="row">
+			<div class="col-7">
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">ABV (Taux d'alcool):</span><span style="font-size: 20px; margin-left: 10px">{{abv}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">target_fg:</span><span style="font-size: 20px; margin-left: 10px">{{target_fg}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">EBC (Indice d'évaluation de la couleur de la biere):</span><span style="font-size: 20px; margin-left: 10px">{{ebc}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">ph:</span><span style="font-size: 20px; margin-left: 10px">{{ph}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">volume:</span><span style="font-size: 20px; margin-left: 10px">{{volume.value}} {{boil_volume.unit}}</span>
+				</div>
+
+			</div>
+			<div class="col-5" style="border-left: solid black 3px">
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">IBU (Taux d'amertume):</span><span style="font-size: 20px; margin-left: 10px">{{ibu}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">target_og:</span><span style="font-size: 20px; margin-left: 10px">{{target_og}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">srm:</span><span style="font-size: 20px; margin-left: 10px">{{srm}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">attenuation_level:</span><span style="font-size: 20px; margin-left: 10px">{{attenuation_level}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">boil_volume:</span><span style="font-size: 20px; margin-left: 10px">{{boil_volume.value}} {{boil_volume.unit}}</span>
+				</div>
+			</div>
+		</div>
+		<div class="text-start mt-4">
+			<h3>Methode:</h3>
+		</div>
+		<div class="row">
+			<div class="col-6">
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">mash temp</span><span style="font-size: 20px; margin-left: 10px">{{method.mash_temp[0].temp.value}} {{method.mash_temp[0].temp.unit}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">mash duration</span><span style="font-size: 20px; margin-left: 10px">{{method.mash_temp[0].duration}}</span>
+				</div>
+			</div>
+			<div class="col-6" style="border-left: solid black 3px">
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">Fermentation température:</span><span style="font-size: 20px; margin-left: 10px">{{method.fermentation.temp.value}} {{method.fermentation.temp.unit}}</span>
+				</div>
+				<div class="text-start mt-2 - mb-2">
+					<span class="fw-bold">Twist:</span><span style="font-size: 20px; margin-left: 10px">{{method.twist}}</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "BeerDisplay",
+	data() {
+		return {
+			id: 192,
+			name: "Punk IPA 2007 - 2010",
+			tagline: "Post Modern Classic. Spiky. Tropical. Hoppy.",
+			first_brewed: "04/2007",
+			description: "Our flagship beer that kick started the craft beer revolution. This is James and Martin's original take on an American IPA, subverted with punchy New Zealand hops. Layered with new world hops to create an all-out riot of grapefruit, pineapple and lychee before a spiky, mouth-puckering bitter finish.",
+			image_url: "https://images.punkapi.com/v2/192.png",
+			abv: 6.0,
+			ibu: 60.0,
+			target_fg: 1010.0,
+			target_og: 1056.0,
+			ebc: 17.0,
+			srm: 8.5,
+			ph: 4.4,
+			attenuation_level: 82.14,
+			volume: {
+				value: 20,
+				unit: "liters"
+			},
+			boil_volume: {
+				value: 25,
+				unit: "liters"
+			},
+			method: {
+				mash_temp: [
+					{
+						temp: {
+							value: 65,
+							unit: "celsius"
+						},
+						duration: 75
+					}
+				],
+				fermentation: {
+					temp: {
+						value: 19.0,
+						unit: "celsius"
+					}
+				},
+				twist: null
+			}
+		};
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
